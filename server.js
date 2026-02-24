827b253b80e560f50c74802a442587ebad35cec4
intellectsoftent <intellectsoftent@gmail.com>
2026-02-24 23:19:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { sequelize } = require("./models");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // ─── Ensure uploads directory exists ──────────────────────────────────────────
 const uploadsDir = path.join(__dirname, "uploads");
